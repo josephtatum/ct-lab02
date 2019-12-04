8e77d4ccb56fe0cd41f1eef6cfeff7b9acb656a6
Joseph Tatum <josephtatum@gmail.com>
2019-12-03 16:17:52 -0800

    all test created and passing for validator

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -13,13 +13,22 @@ describe('validator module', () => {
     expect(weightValidator.validate({ weight: '20 lbs' })).toEqual('20 lbs');
   });
 
-  it('should throw an error if the key is not a name, age or weight', () => {
+  it('should throw an error if the object doesn\'t inlude a required key/value pair', () => {
     expect(() => nameValidator.validate({ color: 'spot', age: 5 })).toThrowErrorMatchingSnapshot();
     expect(() => ageValidator.validate({ color: 'spot', weight: '5 lbs' }).toThrowErrorMatchingSnapshot());
   });
 
+  it('should throw an error if the field is required and is the wrong type', () => {
+    expect(() => nameValidator.validate({ color: 'spot', name: () => {} })).toThrowErrorMatchingSnapshot();
+    expect(() => ageValidator.validate({ color: 'spot', age: () => { } })).toThrowErrorMatchingSnapshot();
+  });
+
+  it('should throw an error if the field isn\'t required and is the wrong type', () => {
+    expect(() => weightValidator.validate({ color: 'spot', weight: () => { } })).toThrowErrorMatchingSnapshot();
+  });
+
   it('should return null if the object key isn\'t included and isn\'t required.', () => {
-    expect(() => weightValidator.validate({ name: 'spot', age: 5 }).toEqual(null));
+    expect(weightValidator.validate({ color: 'spot', age: 5 })).toEqual('Not Required');
   });
 
 });
